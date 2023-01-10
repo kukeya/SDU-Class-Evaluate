009e98f933c8c57a7b8ce9c6ee204e94456a87bc
Kukeya <70995690+kukeya@users.noreply.github.com>
2023-01-10 08:21:26 +0000

    大三上课程Beta

---
+++ sdu-class-evaluate.js
@@ -0,0 +1,10 @@
+for (i = 1; i <= 20; i++) {
+    $(`input[id='pj0601id_${i}_1']`).click()
+}
+var ob = document.querySelectorAll(`input[name='kctzdnd']`)
+$(ob[2]).click()
+ob = document.querySelectorAll(`input[name='yxjspx']`)
+$(ob[0]).click()
+$("#jynr_F9EBEE61A4E14A20B5F1C0DBBB4538D7").val("满意")
+$("#jynr_4F30205F0BD9418EA3A944D7DB8CD3A4").val("满意")
+$("input[id='tj']").click()
